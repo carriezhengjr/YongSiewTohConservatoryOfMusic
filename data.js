48f2554399cdb890105e3bae5419c96d3241331c
Carrie Zheng Jiarui <carriezhengjiarui@gmail.com>
2022-05-13 10:54:32 +0800

    First commit
    YST Conservatory of Music

---
+++ data.js
@@ -0,0 +1,182 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01-alice-lee-plaza",
+      "name": "01 Alice Lee Plaza",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -3.035008525156055,
+        "pitch": -0.2991399127261438,
+        "fov": 1.4999900701183433
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7478517159784346,
+          "pitch": -0.045938594517199505,
+          "rotation": 12.566370614359176,
+          "target": "1-02-conservatory-foyer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.7458749381870557,
+          "pitch": 0.10733249297549818,
+          "title": "Alice Lee Plaza",
+          "text": "Located next to the University Cultural Centre (UCC)."
+        },
+        {
+          "yaw": 2.137150094836219,
+          "pitch": -0.1749986739218823,
+          "title": "NUS Museum",
+          "text": "Home to over 8,000 artworks and artefacts."
+        },
+        {
+          "yaw": -2.4069456577054673,
+          "pitch": -0.10543893668247861,
+          "title": "Welcome to&nbsp;Yong Siew Toh Conservatory of Music!",
+          "text": "Singapore's first conservatory of music."
+        }
+      ]
+    },
+    {
+      "id": "1-02-conservatory-foyer",
+      "name": "02 Conservatory Foyer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.2507374418358026,
+        "pitch": -0.23813262978962513,
+        "fov": 1.4999900701183433
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.16436396213708093,
+          "pitch": 0.05763187495062283,
+          "rotation": 0.7853981633974483,
+          "target": "0-01-alice-lee-plaza"
+        },
+        {
+          "yaw": -2.939997385594154,
+          "pitch": 0.04905383861533252,
+          "rotation": 0.7853981633974483,
+          "target": "2-03-conservatory-lounge"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7972926683391393,
+          "pitch": -0.2194005762154454,
+          "title": "Elegant space with high ceilings",
+          "text": "Suitable for both informal and formal receptions."
+        },
+        {
+          "yaw": 2.391084986853656,
+          "pitch": 0.4596100324336696,
+          "title": "Pa Kua (2012)",
+          "text": "A sculpture by Mr Chong Fahcheong."
+        }
+      ]
+    },
+    {
+      "id": "2-03-conservatory-lounge",
+      "name": "03 Conservatory Lounge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.948261479243545,
+        "pitch": 0.010098848931225035,
+        "fov": 1.4999900701183433
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.32745303698433936,
+          "pitch": 0.05030067968918672,
+          "rotation": 5.497787143782138,
+          "target": "1-02-conservatory-foyer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.0845309075763554,
+          "pitch": 0.2494733848623234,
+          "title": "Conservatory Lounge",
+          "text": "A suitable place for VIP cocktail receptions of not more than 100 persons."
+        },
+        {
+          "yaw": 2.343971340884549,
+          "pitch": 0.5326163641638342,
+          "title": "A resting place",
+          "text": "Take a break at this comfortable lounge."
+        },
+        {
+          "yaw": 1.0327611009918876,
+          "pitch": -0.019516213284688888,
+          "title": "A picture wall",
+          "text": "Photographs of many events."
+        }
+      ]
+    }
+  ],
+  "name": "Yong Siew Toh Conservatory of Music",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
